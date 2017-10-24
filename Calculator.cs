db8e4975035b5a634ed81499483a3c9aac1613d0
Артём Солдатенко < >
2017-10-25 00:40:04 +0500

    Implement func

--- Calculator.cs
+++ Calculator.cs
@@ -12,13 +12,8 @@ namespace Kontur.Courses.Git
 				return lastResult = double.Parse(args[0]);
 			if (args.Length == 2)
 			{
-				// Если не хватает первого аргумента, то использовать lastResult
-				// Должно работать так:
-				// 2 + 2
-				//> 4
-				// + 1
-				//>5
-				return lastResult = Maybe<double>.FromError("Not implemented yet");
+			    var v1 = double.Parse(args[1]);
+			    return lastResult = Execute(args[0], lastResult.Value, v1);
 			}
 			if (args.Length == 3)
 			{
